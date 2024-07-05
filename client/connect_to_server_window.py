255291fb7ddd6d4d703e8df51c0402ed7c37fb2e
AndreyKovalskiy27 <andreykovalskiy27@gmail.com>
2024-07-05 20:39:53 +0300

    Додати модуль для показу повідомленнь та вивод повідомлення про помилку якщо користувач не ввід всі данні для підключення до сервера

--- client/connect_to_server_window.py
+++ client/connect_to_server_window.py
@@ -3,6 +3,7 @@
 
 from PyQt6.QtWidgets import QMainWindow
 from design.connect_to_server import ConnectToServerWindowDesign
+import messages
 
 
 class ConnectToServerWindow(QMainWindow):
@@ -12,3 +13,19 @@ class ConnectToServerWindow(QMainWindow):
 
         self.design = ConnectToServerWindowDesign()
         self.design.setupUi(self)
+
+        self.design.connect_to_server.clicked.connect(self.connect_to_server)
+
+    def connect_to_server(self) -> None:
+        """Підключитися до серверу"""
+        ip = self.design.ip.text()
+        port = self.design.port.text()
+        nikname = self.design.nikname.text()
+
+        if ip.strip() and port.strip() and nikname.strip():
+            pass
+
+        else:
+            messages.show("Введіть вірні данні",
+                          "Заповніть всі поля",
+                          messages.QMessageBox.Icon.Warning)
---
+++ client/messages.py
@@ -0,0 +1,22 @@
+"""Модуль для показу соповіщень на єкран"""
+
+
+from PyQt6.QtWidgets import QMessageBox
+
+
+def show(message: str, description: str=None,
+         icon: QMessageBox.Icon=None,
+         detailed_text: str=None) -> None:
+    """Показує повідомлення"""
+    qmessagebox = QMessageBox()
+
+    if icon:
+        qmessagebox.setIcon(icon)
+
+    qmessagebox.setWindowTitle(message)
+    qmessagebox.setText(description)
+
+    if detailed_text:
+        qmessagebox.setDetailedText(detailed_text)
+
+    qmessagebox.exec()
